6b7bc0f46f0f77fd3a88aa0947ff380ffda7f57a
Mike Nguyen <mnguyen@interapthq.com>
2016-03-29 21:47:14 -0400

    Added listview to display alarms, update real time with firebase

--- app/src/main/java/com/mikenguyen/homesensor/MainActivity.java
+++ app/src/main/java/com/mikenguyen/homesensor/MainActivity.java
@@ -1,31 +1,39 @@
 package com.mikenguyen.homesensor;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> motionAlarms;
+    private ListView motionAlarmListView;
+    private ArrayAdapter<String> motionAlarmAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        //Init
+        motionAlarms = new ArrayList<>();
+        motionAlarmAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, motionAlarms);
+        motionAlarmListView = (ListView)findViewById(R.id.motion_alarm_listview);
+        motionAlarmListView.setAdapter(motionAlarmAdapter);
+        initFirebase();
     }
 
     @Override
@@ -48,5 +56,37 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /** Firebase **/
+    private void initFirebase() {
+        Firebase fb = new Firebase(Config.FIREBASE_MOTION_ALARM_URL);
+        fb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                motionAlarms.add(dataSnapshot.getValue().toString());
+                motionAlarmAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
     }
 }
